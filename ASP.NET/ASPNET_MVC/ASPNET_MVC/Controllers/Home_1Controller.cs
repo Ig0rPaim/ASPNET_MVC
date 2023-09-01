f28a804c14542a66ffa7115a168af04f37eaeac0
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2023-09-01 15:13:46 -0300

    começo do MVC

---
+++ ASP.NET/ASPNET_MVC/ASPNET_MVC/Controllers/Home_1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_MVC.Controllers
+{
+    public class Home_1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
